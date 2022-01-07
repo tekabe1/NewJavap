770dcaadc6776f57d5c82b97fef277a0950584ab
tekabe1 <96458474+tekabe1@users.noreply.github.com>
2022-01-07 01:38:13 -0800

    the third new

---
+++ Tekabe'sProject/src/javaT/Tjava.java
@@ -0,0 +1,14 @@
+package javaT;
+
+public class Tjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=3;
+		int num2=9;
+		int result=num1+num2;
+		System.out.println(result);
+
+	}
+
+}
